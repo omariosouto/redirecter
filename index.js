e3ea2d2442a3bf2b861a5a0987c2b24b30bccc4f
Mario Souto <soutomarios@gmail.com>
2018-11-16 10:34:45 -0200

    update

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = new express()
+
+
+app.get('*/:deeplink', (req,res) => {
+    res.send(`
+        <a href="whatsapp://send?text=${req.params.deeplink}">Aguarde o redirecionamento para o whatsapp...</a>
+        <script>
+            setTimeout(() => {
+                
+                window.location.href = document.querySelector('a').getAttribute('href')
+            }, 1000)
+        </script>
+    `)
+})
+
+app.listen(3004)
